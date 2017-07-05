608072fa5a40c3f8ab1bf63d36a8a5971591de6a
Georgina Hoagland <georgina.hoagland@gmail.com>
2017-07-05 17:43:52 -0400

    single artist pages made

--- browser/react/components/AllAlbums.js
+++ browser/react/components/AllAlbums.js
@@ -1,51 +1,33 @@
 import React, { Component } from 'react';
-import axios from 'axios';
 import { Link } from 'react-router-dom';
 
 
-export default class AllAlbums extends Component {
-
-  constructor(){
-    super();
-    this.state = {
-      albums: []
-    };
-  }
-
-    componentDidMount () {
-    axios.get('/api/albums/')
-      .then(res => res.data)
-      .then(albums => {
-        this.setState({ albums })
-      });
-  }
-
-  render () {
-
-    const albums = this.state.albums;
-    const selectAlbum = this.props.selectAlbum;
-
-    return (
-      <div>
-        <h3>Albums</h3>
-        <div className="row">
-        {
-          albums.map(album => (
-            <div className="col-xs-4" key={ album.id }>
-              <Link className="thumbnail" to={`/albums/${album.id}`}>
-                <img src={ album.imageUrl } />
-                <div className="caption">
-                  <h5>
-                    <span>{ album.name }</span>
-                  </h5>
-                  <small>{ album.songs.length } songs</small>
-                </div>
-              </Link>
-            </div>
-          ))
-        }
-        </div>
+const AllAlbums = props => {
+  const albums = props.albums;
+  return (
+    <div>
+      <h3>Albums</h3>
+      <div className="row">
+      {
+        albums.map(album => (
+          <div className="col-xs-4" key={ album.id }>
+            <Link className="thumbnail" to={`/albums/${album.id}`}>
+              <img src={ album.imageUrl } />
+              <div className="caption">
+                <h5>
+                  <span>{ album.name }</span>
+                </h5>
+                <small>{ album.songs.length } songs</small>
+              </div>
+            </Link>
+          </div>
+        ))
+      }
       </div>
-    );
-  }
+    </div>
+  );
 }
+
+export default AllAlbums;
+
+
--- browser/react/components/SingleArtist.js
+++ browser/react/components/SingleArtist.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 import axios from 'axios';
+import Songs from '../components/Songs';
+import AllAlbums from './AllAlbums';
 import { Link } from 'react-router-dom';
 
 export default class SingleArtist extends Component {
@@ -7,24 +9,39 @@ export default class SingleArtist extends Component {
   constructor(){
     super();
     this.state = {
-      artist: {}
-      };
+      selectedArtist: {},
+      selectedArtistAlbums: [],
+      selectedArtistSongs: []
+    };
+  }
+  componentDidMount() {
+    axios.get(`/api/artists/${this.props.match.params.artistId}`)
+    .then(res => res.data)
+    .then(selectedArtist => {
+        this.setState({ selectedArtist });
+    });
+
+    axios.get(`/api/artists/${this.props.match.params.artistId}/albums`)
+    .then(res => res.data)
+    .then(selectedArtistAlbums => {
+        this.setState({ selectedArtistAlbums });
+    });
+
+    axios.get(`/api/artists/${this.props.match.params.artistId}/songs`)
+    .then(res => res.data)
+    .then(selectedArtistSongs => {
+        this.setState({ selectedArtistSongs });
+    });
   }
 
-  // componentDidMount () {
-  //   axios.get('/api/artists/')
-  //     .then(res => res.data)
-  //     .then(artists => {
-  //       this.setState({ artists });
-  //     });
-  // }
+
 
     render(){
       return (
         <div>
-          <h3>ARTIST NAME</h3>
-          <h4>ALBUMS</h4>
-          <h4>SONGS</h4>
+          <h3>{this.state.selectedArtist.name}</h3>
+          <AllAlbums albums={this.state.selectedArtistAlbums} />
+          <Songs songs={this.state.selectedArtistSongs} />
         </div>
       );
     }
